bf2d85abbbcb8ba6b6f4e81b1a5b512978e73c75
Ivan Semikin <i.semikin.94@icloud.com>
2024-10-10 21:53:11 +0200

    £1

--- CupcakeCorner/Models/Order.swift
+++ CupcakeCorner/Models/Order.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vaville", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnables = false
+    var extraFrosting = false
+    var addSpronkles = false
+}
